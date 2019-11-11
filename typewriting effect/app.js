6b71095979a4ea709e18cbe0ff37041d98df4297
Garbalau Nick <53371076+garbalau-github@users.noreply.github.com>
2019-11-11 09:50:21 +0200

    Add files via upload

---
+++ typewriting effect/app.js
@@ -0,0 +1,26 @@
+const texts = ['websites', 'illustrations', 'pancakes'];
+let count = 0; // 0, 1, 2
+let index = 0; // individual letters
+let currentText = ''; // text what is currently selected
+let letter = ''; // individual letter
+
+// Self invoked every 0.4 seconds
+(function type(){
+
+  if(count === texts.length){
+    count = 0; // reseting
+  }
+
+  currentText = texts[count]; // current word in array
+  letter = currentText.slice(0, ++index); // current character
+
+  document.querySelector('.typing').textContent = letter;
+
+  if(letter.length === currentText.length){ // if word ended
+    count++;
+    index = 0; // reseting to 0
+  }
+
+  setTimeout(type, 400);
+
+})();
